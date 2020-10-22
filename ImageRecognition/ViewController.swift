a02463d005ce25358cc121969fbf702afab1eb71
Christian Lorenzo <clbmiami2004@gmail.com>
2020-10-22 03:52:09 -0700

    Added CoreML Model
    Added camera option to App.

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+//MARK: - Properties:
+    //imagePicker controller object:
+    let imagePicker = UIImagePickerController()
+    
+//MARK: - Outlets:
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary // This could be change to .camera in case we want to use the camera image.
+        imagePicker.allowsEditing = false
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+            imageViewOutlet.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+//MARK: - Action:
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
 }
 
