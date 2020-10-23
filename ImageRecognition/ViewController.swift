0fe50ab04d33aaddcc8cfa65374dfd57c319f55f
Christian Lorenzo <clbmiami2004@gmail.com>
2020-10-23 01:10:45 -0700

    Created a detect Function
    Cerated a "detect function" in order to be able to process the image that was previously converted from UIImage to CIImage to be able to work with the CoreML model.

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreML
 import Vision
+import ImageIO
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -22,17 +23,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary // This could be change to .camera in case we want to use the camera image.
+        imagePicker.sourceType = .camera // This could be change to .library in case we want to use the camera image.
         imagePicker.allowsEditing = false
     }
     
     private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
+        //This image is pciked by the user:
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
             imageViewOutlet.image = userPickedImage
+            
+            //The same image the user picked will get converted into a CIImage
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("UIImage could not be converted into a CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    //Processing images from the imagePickerController:
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: MobileNetV2.urlOfModelInThisBundle)) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
     }
 
 //MARK: - Action:
